0b51e9ab9a0a40a47d7b11fa44f7a0e662f20417
Toyota-InfoTech <45195524+pasta-auto@users.noreply.github.com>
2022-06-27 07:46:05 -0700

    Fix pip package installation issue

--- InstallPython4CARLA.bat
+++ InstallPython4CARLA.bat
@@ -5,9 +5,9 @@ set PYTHON_INSTALLER=python-3.7.9-amd64.exe
 
 pushd "%~dp0"
 if exist %PYTHON_INSTALLER% (
-  start /wait .\%PYTHON_INSTALLER% PrependPath=1
+  start /wait .\%PYTHON_INSTALLER% PrependPath=1 InstallAllUsers=1
   @echo Installing additional python packages for PASTA-CARLA...
-  python -m pip install -r .\requirements.txt
+  runas /user:%USERNAME% "cmd /k python -m pip install -r %~dp0/requirements.txt & timeout /T 10 & exit"
   @echo Done.
 ) else (
   @echo No such a file: %PYTHON_INSTALLER%
